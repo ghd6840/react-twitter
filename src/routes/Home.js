9ab89d7abae289c1a8bd958fd1df6e84152c6b0d
Kyle <ghd6840@naver.com>
2023-07-31 21:23:11 +0900

    Clone Twitter, need to fix userObj to match firebase9

---
+++ src/routes/Home.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { dbService } from "myFirebase";
+import Tweet from "components/Tweet";
+import TweetFactory from "components/TweetFactory";
+
+const Home = ({ userObj }) => {
+  const [tweets, setTweets] = useState([]);
+  useEffect(() => {
+    dbService
+      .collection("tweets")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        const tweetArray = snapshot.docs.map((doc) => ({  //not re-render
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setTweets(tweetArray);
+      });
+  }, []);
+  return (
+    <div className="container">
+      <TweetFactory userObj={userObj} />
+      <div style={{ marginTop: 30 }}>
+        {tweets.map((tweet) => (
+          <Tweet
+            key={tweet.id}
+            tweetObj={tweet}
+            isOwner={tweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Home;
